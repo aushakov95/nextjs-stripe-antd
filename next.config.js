c07ac3203c2b1485dd5d1329e4cbf1d306a7fea1
Andrei Ushakov <ushakoan@gmail.com>
2020-04-28 11:08:55 -0700

    Adjusted config

--- next.config.js
+++ next.config.js
@@ -13,18 +13,14 @@ module.exports = withCSS({
             callback();
           }
         },
-        ...(typeof origExternals[0] === "function" ? [] : origExternals)
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
       ];
 
       config.module.rules.unshift({
         test: antStyles,
-        use: "null-loader"
+        use: "null-loader",
       });
     }
     return config;
   },
-  env: {
-    //STRIPE_SECRET_KEY_TEST: process.env.GOOGLE_API_KEY
-    STRIPE_SECRET_KEY_TEST: "sk_test_XxOczgTCmqMRFYGAlLG2iJD500iQwpEyqB"
-  }
 });
